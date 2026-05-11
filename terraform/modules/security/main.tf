5ac2ee0d50d44ee338a1a0bbf47a034defabb91a
nabixka <nabixka05@gmail.com>
2026-05-11 09:13:36 +0700

    terra

--- terraform/modules/security/main.tf
+++ terraform/modules/security/main.tf
@@ -82,8 +82,15 @@ resource "aws_security_group" "ecs" {
     from_port   = 8080
     to_port     = 8080
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    security_groups = [aws_security_group.alb.id]
   }
+  ingress {
+    from_port   = 5000
+    to_port     = 5000
+    protocol    = "tcp"
+    security_groups = [aws_security_group.alb.id]
+  }
+
   ingress {
     from_port   = 9100
     to_port     = 9100
